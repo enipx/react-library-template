c184cbd963677a40e72b8ee30065c146a13ae839
enipx <progenipx@gmail.com>
2022-12-29 19:02:14 +0100

    feat: add bundler

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,38 +1,6 @@
 import React from 'react';
+import type { ButtonProps, ButtonSizesType } from './Button.types';
 
-type ButtonSizesType = 'small' | 'medium' | 'large';
-
-export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-
-  /**
-   * What color to use
-   */
-  color?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: ButtonSizesType;
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
-}
-
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = 'medium',
